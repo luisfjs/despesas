01ba41e2905625aa4e008f3ba444dbe1d84d4c50
Luis Fernando Joaquim de Sena <lsena@brasilseg.com.br>
2020-04-14 22:14:13 -0300

    ajuste profile local

--- src/main/java/br/com/lsena/despesas/DespesasApplication.java
+++ src/main/java/br/com/lsena/despesas/DespesasApplication.java
@@ -9,5 +9,4 @@ public class DespesasApplication {
     public static void main(String[] args) {
         SpringApplication.run(DespesasApplication.class, args);
     }
-
 }
--- src/main/java/br/com/lsena/despesas/resource/DespesaResource.java
+++ src/main/java/br/com/lsena/despesas/resource/DespesaResource.java
@@ -7,6 +7,7 @@ import br.com.lsena.despesas.dto.TotalPorNomeDto;
 import br.com.lsena.despesas.service.DespesaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +20,13 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DespesaResource {
 
+    private final Environment environment;
     private final DespesaService despesaService;
 
     @LogarExecucao
     @GetMapping
     public ResponseEntity<Despesa> listar(@RequestParam(required = false) String filtro) {
+        System.out.println(environment.getProperty("DB_HOSTNAME"));
         return new ResponseEntity(despesaService.getAll(filtro), HttpStatus.OK);
     }
 
